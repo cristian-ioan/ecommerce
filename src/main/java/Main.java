83f3656e8da88b297d8add980940b896bcd18d64
MihaiMadalina <madalina_mihai93@yahoo.com>
2018-11-24 14:46:24 +0200

    fix Main class after conflict test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,4 @@
 public class Main {
-import product.ProductConsoleWriter;
-
-
-
     public static void main(String[] args) {
         ConsoleMenu consoleMenu = new ConsoleMenu();
         consoleMenu.displayConsoleMenu();
