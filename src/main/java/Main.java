519be57778ff38cdfd8edff87271d61072d1c88a
Cristian Lucian IOAN <cristian-ioan@users.noreply.github.com>
2018-11-24 14:36:38 +0200

    fine tuning Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,5 @@
-import product.ProductFileStorage;
-
-
 public class Main {
+
     public static void main(String[] args) {
         ConsoleMenu consoleMenu = new ConsoleMenu();
         consoleMenu.displayConsoleMenu();
